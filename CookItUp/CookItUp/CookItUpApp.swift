d99bb0519bf465369c53d98b8361717e1349e824
Arina Knaub <iamknaubarina@gmail.com>
2024-05-29 16:39:58 +0200

    Add Project

---
+++ CookItUp/CookItUp/CookItUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookItUpApp.swift
+//  CookItUp
+//
+//  Created by Arina Knaub on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CookItUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
