553afbb1c86a93a7c44d4c91f94bd657f7ae0c9d
Arina <iamknaubarina@gmail.com>
2024-06-02 17:15:17 +0200

    Create Recipe Card View

--- CookItUp/CookItUp/CookItUpKit/RecipeCard/RecipeCardView.swift
+++ CookItUp/CookItUp/CookItUpKit/RecipeCard/RecipeCardView.swift
@@ -4,89 +4,16 @@ import SwiftUI
 
 struct RecipeCardView: View {
     var body: some View {
-        GeometryReader { geometry in
-            let fixedWidth = geometry.size.width * 1
-            let fixedHeight = fixedWidth * 3 / 4
-            
-            VStack {
-                ZStack {
-                    Image(uiImage: .recipePicture1)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: fixedWidth, height: fixedHeight)
-                        .clipped()
-                    
-                    VStack {
-                        HStack {
-                            Text("Popular")
-                                .typography(.caption3)
-                                .frame(width: 67, height: 28)
-                                .background(Color.neutral01)
-                                .clipShape(RoundedRectangle(cornerRadius: 12))
-                            
-                            Spacer()
-                            
-                            Button {
-                                // logic add to fav
-                            } label: {
-                                Image(uiImage: .favoritesInactiveCard)
-                            }
-                        }
-                        .padding([.leading, .trailing], 16)
-                        .padding(.top, 16)
-                        
-                        Spacer()
-                        
-                        Text("Cottage Cheese Pancakes With Berries & Sour Cream")
-                            .typography(.heading2)
-                            .foregroundColor(.neutral01)
-                            .padding([.leading, .trailing], 16)
-                            .padding(.bottom, 16)
-                    }
-                    .frame(width: fixedWidth, height: fixedHeight, alignment: .top)
-                }
-                
-                HStack(spacing: 12) {
-                    HStack(spacing: 8) {
-                        Image(uiImage: .timer)
-                        Text("30 min")
-                    }
-                    
-                    Divider()
-                        .frame(height: 24)
-                    
-                    HStack(spacing: 8) {
-                        Image(uiImage: .complexity)
-                        Text("Easy")
-                    }
-                    
-                    Divider()
-                        .frame(height: 24)
-                    
-                    HStack(spacing: 8) {
-                        Image(uiImage: .servings)
-                        Text("2 sv")
-                    }
-                    
-                    Divider()
-                        .frame(height: 24)
-                    
-                    HStack(spacing: 8) {
-                        Image(uiImage: .calories)
-                        Text("938 Cal")
-                    }
-                }
-                .padding(.vertical, 8)
-                .frame(maxWidth: .infinity)
-                .background(.neutral03)
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-//            .clipShape(RoundedRectangle(cornerRadius: 12))
-//            .shadow(color: .black.opacity(0.12), radius: 16, x: 0, y: 6)
+        VStack(spacing: 0) {
+            RecipeCardImageView()
+            RecipeCardInfoView()
         }
+        .frame(height: 259)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .shadow(color: .neutral05.opacity(0.12), radius: 16, y: 6)
+        .padding(.horizontal)
     }
 }
-
 
 #Preview {
     RecipeCardView()
---
+++ CookItUp/CookItUp/CookItUpKit/RecipeCard/ReusableViews/RecipeCardImageView/RecipeCardImageView.swift
@@ -0,0 +1,48 @@
+//  Created by Arina Knaub on 02/06/2024.
+
+import SwiftUI
+
+struct RecipeCardImageView: View {
+    var body: some View {
+        ZStack(alignment: .top) {
+            Image(uiImage: .recipePicture1)
+                .resizable()
+                .scaledToFill()
+                .frame(height: 219)
+                .clipped()
+            
+            LinearGradient(
+                colors: [.neutral05.opacity(0), .neutral05.opacity(0.75)],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Popular")
+                        .typography(.caption3)
+                        .padding(.vertical, 4)
+                        .padding(.horizontal, 10)
+                        .background(Capsule().foregroundStyle(.neutral01))
+                        .foregroundStyle(.neutral05)
+                    Spacer()
+                    Button {
+                        // logic add to fav
+                    } label: {
+                        Image(uiImage: .favoritesInactiveCard)
+                    }
+                }
+                
+                Spacer()
+                
+                Text("Cottage Cheese Pancakes With Berries & Sour Cream")
+                    .typography(.heading2)
+                    .foregroundStyle(.neutral01)
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    RecipeCardImageView()
+}
