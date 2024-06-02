3a9da860334b141ec247611488243a7d7d4d6729
Arina <iamknaubarina@gmail.com>
2024-06-02 21:09:12 +0200

    Add Project Structure

---
+++ CookItUp/CookItUp/Features/Home/Screens/Home/HomeView.swift
@@ -0,0 +1,13 @@
+//  Created by Arina Knaub on 02/06/2024.
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
