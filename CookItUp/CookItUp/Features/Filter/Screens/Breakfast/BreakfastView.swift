3db221cba1ca8926b3f2aa668a39ada59496afff
Arina <iamknaubarina@gmail.com>
2024-06-05 18:16:22 +0200

    Create Five Filter Recipe Views

---
+++ CookItUp/CookItUp/Features/Filter/Screens/Breakfast/BreakfastView.swift
@@ -0,0 +1,26 @@
+//  Created by Arina Knaub on 05/06/2024.
+
+import SwiftUI
+
+struct BreakfastView: View {
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 16) {
+                ForEach(1...10, id: \.self) { recipe in
+                    VStack(spacing: 0) {
+                        RecipeCardImageView()
+                        RecipeCardInfoView()
+                    }
+                }
+                .frame(height: 259)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                .shadow(color: .neutral05.opacity(0.12), radius: 16, y: 6)
+                .padding(.horizontal)
+            }
+        }
+    }
+}
+
+#Preview {
+    BreakfastView()
+}
