74885ebe63e5946a36b6d85052a44844fc6e977d
Arina <iamknaubarina@gmail.com>
2024-06-08 12:17:21 +0200

    Set up Firebase

---
+++ CookItUp/CookItUp/App/AppDelegate.swift
@@ -0,0 +1,14 @@
+//  Created by Arina Knaub on 29/05/2024.
+
+import FirebaseCore
+import SwiftUI
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
