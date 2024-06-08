5adbbaf1b62dce8a9c5ae47a1dcbe75a4e535ea3
Arina <iamknaubarina@gmail.com>
2024-06-08 12:44:52 +0200

    Create Recipe Models

---
+++ CookItUp/CookItUp/Models/Recipe/RecipeStep.swift
@@ -0,0 +1,8 @@
+//  Created by Arina Knaub on 08/06/2024.
+
+import Foundation
+
+struct RecipeStep {
+    let imageURL: String
+    let description: String
+}
---
+++ CookItUp/CookItUp/Models/Recipe/RecipeType.swift
@@ -0,0 +1,10 @@
+//  Created by Arina Knaub on 08/06/2024.
+
+import Foundation
+
+enum RecipeType: String {
+    case breakfast = "Breakfast"
+    case lunch = "Lunch"
+    case dinner = "Dinner"
+    case dessert = "Dessert"
+}
